efcb5da56f2ee09698903a4a29e80518bda1f070
AbdelmjeednErfan <abdelmjeednerfan@gmail.com>
2025-02-12 20:50:37 +0200

    first commit

---
+++ src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+export default function NotFound() {
+  return (
+    <div className=' container '>
+      <div className="row">
+        <div className="col-md-12 text-center">
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+          <h1>Not Found</h1>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
---
+++ src/Components/Portfolio/Portfolio.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+export default function Portfolio() {
+  return (
+    <div>
+      <div className="container py-5">
+        <div className="row">
+          <div className="parent flex-column my-5 text-black d-flex justify-content-start align-items-center  vh-100">
+            <div className="textPortfolio text-center text-uppercase fw-bold">
+              <h1>portfolio component</h1>
+            </div>
+            <div className="icon d-flex justify-content-center align-items-center ">
+              <div className="line2"></div>
+              <i className="fa-solid fa-star m-3 "></i>
+              <div className="line2"></div>
+            </div>
+            <div className="row g-5">
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src="/images/portHome.png" alt="portHome" className="itemImage    " />
+                </div>
+              </div>
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src="/images/portCake.png" alt="portCake" className="itemImage  " />
+                </div>
+              </div>
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src="/images/portCircs.png" alt="portCircs" className="itemImage  " />
+                </div>
+              </div>
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src="/images/portHome.png" alt="portHome" className="itemImage  " />
+                </div>
+              </div>
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src="/images/portCake.png" alt="portCake" className="itemImage  " />
+                </div>
+              </div>
+              <div className="col-md-4 ">
+                <div className="image rounded-3 overflow-hidden ">
+                  <img src='/images/portCircs.png' alt="portCircs" className="itemImage  " />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
